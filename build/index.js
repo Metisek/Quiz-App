8d8e4747354816ef513d3697fcfa05ca547857e2
Mateusz Bojarski <matboj5@gmail.com>
2024-03-12 23:08:52 +0100

    Working TypeScript

--- build/index.js
+++ build/index.js
@@ -1,14 +1,37 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
-const PORT = 3000;
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
-app.listen(PORT, () => {
-    console.log(`Express server is listening at ${PORT}`);
-});
+exports.AppController = void 0;
+const common_1 = require("@nestjs/common");
+let AppController = class AppController {
+    constructor(databaseService) {
+        this.databaseService = databaseService;
+    }
+    getData() {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield this.databaseService.connect();
+            yield this.databaseService.query('SELECT * FROM quizapp.quiz');
+        });
+    }
+};
+__decorate([
+    (0, common_1.Get)()
+], AppController.prototype, "getData", null);
+AppController = __decorate([
+    (0, common_1.Controller)()
+], AppController);
+exports.AppController = AppController;
